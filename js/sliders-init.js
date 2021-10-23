77643be04f0456f7154b852dd01ea8abf8ec0a8e
Leonid <bychkov.l47@mail.ru>
2021-10-23 20:13:33 +0300

    add images

---
+++ js/sliders-init.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper.actions-slider', {
+    speed: 400,
+    spaceBetween: 100,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    grabCursor: true,
+});
